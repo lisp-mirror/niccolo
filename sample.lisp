9bce576fd71bab30f0428ada42a6bed733a5c458
cage <cage@katamail.com>
2018-06-13 17:36:03 +0200

    - updated licence.

--- sample.lisp
+++ sample.lisp
@@ -1,9 +1,10 @@
 ; niccolo': a chemicals inventory
 ;; Copyright (C) 2016  Universita' degli Studi di Palermo
 
-;; This program is free software: you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License.
+;; This  program is  free  software: you  can  redistribute it  and/or
+;; modify it  under the  terms of  the GNU  General Public  License as
+;; published by the Free Software  Foundation, either version 3 of the
+;; License, or (at your option) any later version.
 
 ;; This program is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
